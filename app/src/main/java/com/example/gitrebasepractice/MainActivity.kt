ec04422aa552a9265c89af5c02293123bdd54b33
lucifer@2019 <cm20142019@gmail.com>
2019-08-21 20:35:47 -0500

    second commit

--- app/src/main/java/com/example/gitrebasepractice/MainActivity.kt
+++ app/src/main/java/com/example/gitrebasepractice/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
 //first commit
 //second commit
+//third commit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
